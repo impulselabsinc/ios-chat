dd521c354a8f696578ae331727847cb48e65ab55
BuildTools <unconfigured@null.spigotmc.org>
2017-07-19 23:31:51 -0400

    removed Pods dir

--- Pods/Target Support Files/Pods-ios-chatTests/Pods-ios-chatTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ios_chatTests : NSObject
-@end
-@implementation PodsDummy_Pods_ios_chatTests
-@end
--- Pods/Target Support Files/Pods-ios-chatUITests/Pods-ios-chatUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ios_chatUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ios_chatUITests
-@end
