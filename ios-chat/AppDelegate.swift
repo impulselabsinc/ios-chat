688b48249cb22d16fff95de51dcdb7d58585b616
BuildTools <unconfigured@null.spigotmc.org>
2017-07-25 14:18:59 -0400

    added Google identity management

--- ios-chat/AppDelegate.swift
+++ ios-chat/AppDelegate.swift
@@ -14,85 +14,100 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     var window: UIWindow?
 
 
+    // Google URL Handler
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey:Any] = [:]) -> Bool {
+        
+        // put in handlers for any supported identity provider
+        let googleHandler = GIDSignIn.sharedInstance().handle(
+            url,
+            sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        
+        // return the openURL result from any one identity provider
+        return googleHandler
+    }
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Initialize sign-in
+        // Start Google SignIn modifications
         var configureError: NSError?
         GGLContext.sharedInstance().configureWithError(&configureError)
-        assert(configureError == nil, "Error configuring Google services: \(String(describing: configureError))")
+        assert(configureError == nil, "Error configuring Google services \(String(describing: configureError))")
         
         GIDSignIn.sharedInstance().delegate = self
+        // End Google SignIn modifications
+        
+        //add any other identity provider launch options
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    // Google SignIn methods
-    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
-                                                    annotation: options[UIApplicationOpenURLOptionsKey.annotation])
-    }
-    
-    
-    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
-                withError error: NSError!) {
-        if (error == nil) {
-            // Perform any operations on signed in user here.
-            let userId = user.userID                  // For client-side use only!
-            let idToken = user.authentication.idToken // Safe to send to the server
-            let fullName = user.profile.name
-            let givenName = user.profile.givenName
-            let familyName = user.profile.familyName
-            let email = user.profile.email
-            // ...
+        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
+            GIDSignIn.sharedInstance().signOut()
             
-            print("userId \(String(describing: userId))")
-            print("idToken \(String(describing: idToken))")
-            print("fullName \(String(describing: fullName))")
-            print("givenName \(String(describing: givenName))")
-            print("familyName \(String(describing: familyName))")
-            print("email \(String(describing: email))")
-            print("Wow our user is signed in! \(user))")
-        } else {
-            print("\(error.localizedDescription)")
         }
     }
     
-    func signIn(signIn: GIDSignIn!, didDisconnectWithUser user:GIDGoogleUser!,
-                withError error: NSError!) {
-        // Perform any operations when the user disconnects from app here.
-        // ...
+    // MARK: Google SignIn methods
+    func sign(_ signIn:GIDSignIn!, didSignInFor user:GIDGoogleUser!, withError error: Error!){
+        if let error = error {
+            print("\(error.localizedDescription)")
+            
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+                object: nil,
+                userInfo: nil)
+            
+        } else{
+            
+            var imageUrl = ""
+            if user.profile.hasImage{
+                imageUrl = user.profile.imageURL(withDimension: 100).absoluteString
+            }
+            
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+                object: nil,
+                userInfo: ["userID":user.userID,
+                           "profileName":user.profile.name,
+                           "profileEmail":user.profile.email,
+                           "profileGivenName": user.profile.givenName,
+                           "profileFamilyName":user.profile.familyName,
+                           "profileHasImage": user.profile.hasImage,
+                           "profileImageUrl":imageUrl])
+        }
     }
     
-    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!){
         
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+            object: nil,
+            userInfo: nil)
     }
     
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        
-    }
 
 }
 
--- ios-chat/ViewController.swift
+++ ios-chat/ViewController.swift
@@ -10,17 +10,105 @@ import UIKit
 
 class ViewController: UIViewController , GIDSignInUIDelegate{
 
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var loginMessage: UILabel!
+    fileprivate let signOutButton: UIBarButtonItem = UIBarButtonItem(title: nil, style: .done, target: nil, action: nil)
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        GIDSignIn.sharedInstance().uiDelegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Google SignIn
+        setupSignOutButton()
+        disconnectButton.isHidden = true
+        GIDSignIn.sharedInstance().uiDelegate = self
+        //GIDSignIn.sharedInstance().signInSilently()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(ViewController.receiveToggleAuthUINotification(_:)),
+                                               name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
+                                               object:nil)
+        
+        //color the view
+        self.view.backgroundColor = UIColor(red: 255/255, green: 250/255, blue: 205/255, alpha: 1.0)
+        // color the navbar
+        navBar.topItem?.title = "Impulse Labs Demo"
+        navBar.topItem?.prompt = "What would you like to do today?"
+        navBar.backgroundColor = UIColor(red: 150/255, green: 195/255, blue: 150/255, alpha: 1.0)
+        
+        //signInButton.colorScheme = GIDSignInButtonColorScheme.light
+    
+        toggleAuthUI()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    //MARK: Google Auth funcs
+    func setupSignOutButton(){
+        navBar.topItem?.rightBarButtonItem = signOutButton
+        navBar.topItem?.rightBarButtonItem?.target = self
+        navBar.topItem?.rightBarButtonItem?.action = #selector(ViewController.signOut(_:))
+    }
+    
+    func toggleAuthUI(){
+        GIDSignIn.sharedInstance().scopes = ["https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/userinfo.profile","https://www.googleapis.com/auth/plus.me"]
+        if GIDSignIn.sharedInstance().hasAuthInKeychain(){
+            //signed in
+            self.view.backgroundColor = UIColor(red: 150/255, green: 200/255, blue: 150/255, alpha: 1.0)
+            navBar.topItem?.rightBarButtonItem?.isEnabled = true
+            navBar.topItem?.rightBarButtonItem?.title = "Sign Out"
+            signInButton.isHidden = true
+            if let profileName = UserDefaults.standard.string(forKey: "profileName") {
+                loginMessage.textColor = UIColor(red: 255/255, green: 69/255, blue: 0/255, alpha: 1.0)
+                loginMessage.text = "You are logged in as \n\(profileName)"
+            }
+            
+            
+        }else{
+            //signed out
+            self.view.backgroundColor = UIColor(red: 255/255, green: 200/255, blue: 200/255, alpha: 1.0)
+            navBar.topItem?.rightBarButtonItem?.isEnabled = false
+            navBar.topItem?.rightBarButtonItem?.title = ""
+            signInButton.isHidden = false
+            loginMessage.textColor = UIColor(red: 255/255, green: 69/255, blue: 0/255, alpha: 1.0)
+            loginMessage.text = "You are not logged in"
+        }
+    }
+    
+    func signOut(_ sender: AnyObject){
+        GIDSignIn.sharedInstance().signOut()
+        UserDefaults.standard.removeObject(forKey: "profileName")
+        toggleAuthUI()
+    }
+    
+    @IBAction func disconnectAccount(_ sender: Any) {
+        GIDSignIn.sharedInstance().disconnect()
+        UserDefaults.standard.removeObject(forKey: "profileName")
+        toggleAuthUI()
+    }
+    
+    @objc func receiveToggleAuthUINotification(_ notification: NSNotification){
+        if notification.name.rawValue == "ToggleAuthUINotification" {
+            
+            if notification.userInfo != nil {
+                if let username = notification.userInfo?["profileName"] as? String {
+                    UserDefaults.standard.setValue(username, forKey: "profileName")
+                }
+                
+            }
+            self.toggleAuthUI()
+        }
+    }
 
 }
 
